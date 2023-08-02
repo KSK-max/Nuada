856dbe4854be26a34da65ba2bdb178d2ffc2116e
VivoCodes <vivek.raj2@mail.dcu.ie>
2023-08-02 18:36:31 +0100

    baby2

--- Anna-Server-Side/index.js
+++ Anna-Server-Side/index.js
@@ -11,8 +11,15 @@ app.use(cors(
       origin: ["https://nuada-frontend-nm6xpq3ex-ksk-max.vercel.app"],
       methods: ["POST", "GET"],
       credentials: true
+      
   }
 ));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get("/", (req, res) => {
   res.json("Hellos");
