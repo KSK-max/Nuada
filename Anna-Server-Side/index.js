b0813ccad218f216d37cedbf313f5c1e202d40cf
Alan Abraham <alabraha@tcd.ie>
2023-08-03 14:48:10 +0100

    still trying to fix cors issue

--- Anna-Server-Side/index.js
+++ Anna-Server-Side/index.js
@@ -3,36 +3,12 @@ const nodemailer = require("nodemailer")
 const cors = require("cors");
 const app3 = express();
 const corsOptions = {
-  origin: "https://nuada-frontend.vercel.app",
+  origin: "*",
   methods: ["POST", "GET"],
   credentials: true,
 };
+app3.use(cors({corsOptions}));
 
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
-
-const handler = (req, res) => {
-  const d = new Date()
-  res.end(d.toString())
-}
-
-module.exports = allowCors(handler)
-
-app3.use(cors(corsOptions));
 // const mongoose = require("mongoose");
 // const app2 = express();
 // app2.use(express.json());
