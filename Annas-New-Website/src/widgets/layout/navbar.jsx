92c84a994ca8bb2d176b3b66a46b5c1cf0e05e45
Bartlomiej Tutak <barte77pl@gmail.com>
2023-08-06 22:17:58 +0200

    basic layout

--- Annas-New-Website/src/widgets/layout/navbar.jsx
+++ Annas-New-Website/src/widgets/layout/navbar.jsx
@@ -1,139 +1,28 @@
-import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
+import React from "react";
 import { Link } from "react-router-dom";
-import {
-  Navbar as MTNavbar,
-  MobileNav,
-  Typography,
-  Button,
-  IconButton,
-} from "@material-tailwind/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
-export function Navbar({ brandName, routes, action }) {
-  const [openNav, setOpenNav] = useState(false);
-  // const [showBottomButton, setShowBottomButton] = useState(false);
-
-  useEffect(() => {
-    window.addEventListener(
-      "resize",
-      () => window.innerWidth >= 960 && setOpenNav(false)
-    );
-
-    return () => {
-      window.removeEventListener(
-        "resize",
-        () => window.innerWidth >= 960 && setOpenNav(false)
-      );
-    };
-  }, []);
-
-  const handleBottomButtonClick = () => {
-    const formPosition = document
-      .querySelector("#fraudSection")
-      .getBoundingClientRect().top;
-    const yOffset = -100; // Adjust this value to scroll a bit above the top of the section
-    const y = formPosition + window.pageYOffset + yOffset;
-
-    window.scrollTo({ top: y, behavior: "smooth" });
-  };
-  const navList = (
-    <ul className="mb-4 mt-2 flex flex-col gap-2 text-inherit lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-      {routes.map(({ name, path, icon, href, target }) => (
-        <Typography
-          key={name}
-          as="li"
-          variant="small"
-          color="inherit"
-          className="capitalize"
-        >
-          {href ? (
-            <a
-              href={href}
-              target={target}
-              className="flex items-center gap-1 p-1 font-normal"
-            >
-              {icon &&
-                React.createElement(icon, {
-                  className: "w-[18px] h-[18px] opacity-75 mr-1",
-                })}
-              {name}
-            </a>
-          ) : (
-            <Link
-              to={path}
-              target={target}
-              className="flex items-center gap-1 p-1 font-normal"
-            >
-              {icon &&
-                React.createElement(icon, {
-                  className: "w-[18px] h-[18px] opacity-75 mr-1",
-                })}
-              {name}
-            </Link>
-          )}
-        </Typography>
-      ))}
-    </ul>
-  );
-
+const navbar = () => {
   return (
-    <MTNavbar color="transparent" className="p-3">
-      <div className="container mx-auto flex items-center justify-between text-white">
-        <Link to="/">
-          <Typography className="ml-2 mr-4 cursor-pointer py-1.5 font-bold">
-            {brandName}
-          </Typography>
-        </Link>
-        <div className="hidden lg:block">{navList}</div>
-        <div className="hidden lg:block">
-          {/* Add the red button */}
-          <Button
-            size="regular"
-            onClick={handleBottomButtonClick}
-            className="ml-4 bg-secondary-magenta !shadow-secondary-magenta"
-          >
-            Report Fraud
-          </Button>
-        </div>
-        <IconButton
-          variant="text"
-          size="sm"
-          color="white"
-          className="ml-auto text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-          onClick={() => setOpenNav(!openNav)}
-        >
-          {openNav ? (
-            <XMarkIcon strokeWidth={2} className="h-6 w-6" />
-          ) : (
-            <Bars3Icon strokeWidth={2} className="h-6 w-6" />
-          )}
-        </IconButton>
+    <nav className="flex w-screen">
+      <div>
+        <img src="" alt="logo" />
       </div>
-      <MobileNav
-        className="rounded-xl bg-white px-4 pb-4 pt-2 text-blue-gray-900"
-        open={openNav}
-      ></MobileNav>
-    </MTNavbar>
+      <ul className="flex">
+        <li>
+          <Link href="/home">Home</Link>
+        </li>
+        <li>
+          <Link href="/solutions">Solutions</Link>
+        </li>
+        <li>
+          <Link href="/resources">Resources</Link>
+        </li>
+        <li>
+          <Link href="/about">About us</Link>
+        </li>
+      </ul>
+    </nav>
   );
-}
-
-Navbar.defaultProps = {
-  brandName: "Nuada: Chargeback and Dispute Management",
-  action: (
-    <a
-      href="https://www.creative-tim.com/product/material-tailwind-kit-react"
-      target="_blank"
-    ></a>
-  ),
 };
 
-Navbar.propTypes = {
-  brandName: PropTypes.string,
-  routes: PropTypes.arrayOf(PropTypes.object).isRequired,
-  action: PropTypes.node,
-};
-
-Navbar.displayName = "/src/widgets/layout/navbar.jsx";
-
-export default Navbar;
+export default navbar;
