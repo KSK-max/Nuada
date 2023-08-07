a2d96cf1e0f2c7eee38c726eca3dd71609feb73f
Bartlomiej Tutak <barte77pl@gmail.com>
2023-08-08 00:30:30 +0200

    fix success url

--- Anna-Server-Side/index.js
+++ Anna-Server-Side/index.js
@@ -124,8 +124,8 @@ app.post("/create-checkout-session", async (req, res) => {
 		payment_method_types: ["card"],
 		line_items: body,
 		mode: "payment",
-		success_url: `https://nuada-frontend.vercel.app/success`,
-		cancel_url: `https://nuada-frontend.vercel.app`,
+		success_url: `https://wenuada.ie/#/success`,
+		cancel_url: `https://wenuada.ie`,
 	};
 
 	try {
